cb98e68fc2ffb702beff8230bae6696c64d7b75b
Unknown <unknown@example.com>
2020-10-07 19:48:50 +0900

    Support coloring error message

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,18 @@
+const COLOR_RESET: &str = "\x1b[0m";
+const COLOR_RED: &str = "\x1b[31m";
+const COLOR_CYAN: &str = "\x1b[36m";
+
+
 pub fn error(err: &str) {
-    eprintln!("{}", err);
+    eprintln!("{}{}{}", COLOR_RED, err, COLOR_RESET);
     std::process::exit(1);
 }
 
 pub fn error_at(code: &str, i: usize, err: &str) {
-    eprintln!("{}", code);
-    eprintln!("{}^ {}", " ".repeat(i), if i >= code.len() { "unexpected eof while parsing" } else { err });
+    eprintln!("{}{}{}", COLOR_CYAN, code, COLOR_RESET);
+    eprintln!("{}^ {}{}{}", " ".repeat(i),
+              COLOR_RED,
+              if i >= code.len() { "unexpected eof while parsing" } else { err },
+              COLOR_RESET);
     std::process::exit(1);
 }
