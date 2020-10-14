b743339e6efd48db0468bd1085a17d89dd7d6ddb
Unknown <unknown@example.com>
2020-10-14 16:56:25 +0900

    Update test

---
+++ test/string.c
@@ -0,0 +1,13 @@
+int value;
+char *fmt2;
+
+int main() {
+    value = 777;
+    printf("a = %d, ", value);
+    char *fmt = "b = %d, ";
+    value = 755;
+    printf(fmt, value);
+    fmt2 = "c = %d\n";
+    printf(fmt2, 222);
+    return 0;
+}
--- test/test.sh
+++ test/test.sh
@@ -5,17 +5,20 @@ options=$1
 assert() {
   expected="$1"
   input="$2"
-  ./target/debug/dynamite_compiler "$input" > ./temp/main.s
+  assembly=`./target/debug/dynamite_compiler "$input"`
   if [ $? != 0 ]; then
     exit 1
   fi
-  cc $options -o ./temp/main ./temp/main.s
+  echo "$assembly" | cc $options -x assembler -o ./temp/main -
   if [ $? != 0 ]; then
     exit 1
   fi
-  ./temp/main
-  actual="$?"
-
+  if [ "$3" = "stdout" ]; then
+    actual=`./temp/main`
+  else
+    ./temp/main
+    actual="$?"
+  fi
   if [ "$actual" = "$expected" ]; then
     echo "$input => $actual"
   else
@@ -24,37 +27,12 @@ assert() {
   fi
 }
 
-assert_stdout() {
-  expected="$1"
-  input="$2"
-  ./target/debug/dynamite_compiler "$input" > ./temp/main.s
-  if [ $? != 0 ]; then
-    exit 1
-  fi
-  cc $options -o ./temp/main ./temp/main.s
-  if [ $? != 0 ]; then
-    exit 1
-  fi
-  actual=`./temp/main`
-
-  if [ "$actual" = "$expected" ]; then
-    echo "( $input ) => ( $actual )"
-  else
-    echo "[ $input ) => ( $expected ) expected, but got ( $actual )"
-    exit 1
-  fi
-}
-
 if [ ! -d ./temp ]; then
   mkdir ./temp
 fi
-<< CMT
-CMT
 
 assert 0 './test/expr.c'
-assert 0 './test/test.c'
-assert_stdout "value = 777" 'int value; int main() { value = 777; printf("value = %d\n", value); return 0;}'
-assert_stdout "value = 755" 'int value; int main() { char* fmt = "value = %d\n"; value = 755; printf(fmt, value); return 0;}'
-assert_stdout "value = 222" 'char* fmt; int main() { fmt = "value = %d\n"; printf(fmt, 222); return 0;}'
+assert 0 './test/functions.c'
+assert "a = 777, b = 755, c = 222" './test/string.c' stdout
 
 echo OK
