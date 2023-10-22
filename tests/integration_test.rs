3d08831c45f9a43f226aa75ed73e990470bf9801
dytlzl <dytlzl.dev@gmail.com>
2023-10-22 19:35:11 +0900

    Fix integration_test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -8,28 +8,28 @@ extern crate rand;
 use rand::distributions::{Alphanumeric, DistString};
 use test_case::test_matrix;
 
-#[test_matrix(["ast", "llvm"])]
+#[test_matrix(["asm", "llvm"])]
 fn it_defines_one_global_variable(output_option: &str) {
     let code = "int a = 7; int main() { printf(\"%d\", a); return 0; }";
     let got = compile_and_get_stdout(code, output_option);
     assert_eq!(got, "7")
 }
 
-#[test_matrix(["ast", "llvm"])]
+#[test_matrix(["asm", "llvm"])]
 fn it_compiles_simple_c(output_option: &str) {
     let code = &fs::read_to_string("./tests/c/simple.c").unwrap();
     let got = compile_and_get_stdout(code, output_option);
     assert_eq!(got, "12, 2, 35, 5, 7\n")
 }
 
-#[test_matrix(["ast", "llvm"])]
+#[test_matrix(["asm", "llvm"])]
 fn it_compiles_simple_function_c(output_option: &str) {
     let code = &fs::read_to_string("./tests/c/simple_function.c").unwrap();
     let got = compile_and_get_stdout(code, output_option);
     assert_eq!(got, "10\n")
 }
 
-#[test_matrix(["ast", "llvm"])]
+#[test_matrix(["asm", "llvm"])]
 fn it_compiles_character_constant_c(output_option: &str) {
     let code = &fs::read_to_string("./tests/c/character_constant.c").unwrap();
     let got = compile_and_get_stdout(code, output_option);
@@ -37,14 +37,14 @@ fn it_compiles_character_constant_c(output_option: &str) {
 }
 
 #[cfg(target_arch = "x86_64")]
-#[test_matrix(["ast", "llvm"])]
+#[test_matrix(["asm", "llvm"])]
 fn it_compiles_string_c(output_option: &str) {
     let code = &fs::read_to_string("./tests/c/string.c").unwrap();
     let got = compile_and_get_stdout(code, output_option);
     assert_eq!(got, "a = 777, b = 755, c = 222\n")
 }
 
-#[test_matrix(["ast", "llvm"])]
+#[test_matrix(["asm", "llvm"])]
 fn it_compiles_simple_expr_c(output_option: &str) {
     let code = &fs::read_to_string("./tests/c/simple_expr.c").unwrap();
     let got = compile_and_get_stdout(code, output_option);
@@ -54,7 +54,7 @@ fn it_compiles_simple_expr_c(output_option: &str) {
 }
 
 #[cfg(target_arch = "x86_64")]
-#[test_matrix(["ast", "llvm"])]
+#[test_matrix(["asm"])]
 fn it_compiles_expr_c(output_option: &str) {
     let code = &fs::read_to_string("./tests/c/expr.c").unwrap();
     let got = compile_and_get_stdout(code, output_option);
@@ -64,7 +64,7 @@ fn it_compiles_expr_c(output_option: &str) {
 }
 
 #[cfg(target_arch = "x86_64")]
-#[test_matrix(["ast", "llvm"])]
+#[test_matrix(["asm"])]
 fn it_compiles_many_functions_c(output_option: &str) {
     let code = &fs::read_to_string("./tests/c/many_functions.c").unwrap();
     let got = compile_and_get_stdout(code, output_option);
@@ -79,7 +79,16 @@ fn compile_and_get_stdout(code: &str, output_option: &str) -> String {
     fs::create_dir_all("./tests/temp").unwrap();
     let binary_name = &format!("./tests/temp/{}", Alphanumeric.sample_string(&mut rng, 32));
     let child = Command::new("cc")
-        .args(["-x", "ir", "-o", binary_name, "-"])
+        .args([
+            "-x",
+            match output_option {
+                "asm" => "assembler",
+                _ => "ir",
+            },
+            "-o",
+            binary_name,
+            "-",
+        ])
         .stdin(std::process::Stdio::piped())
         .stdout(std::process::Stdio::piped())
         .stderr(std::process::Stdio::piped())
